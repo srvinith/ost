07854887fc870e65a5a2f62c4ff75af5a74bc7c1
Isaak29 <akmohamedisaakmuzamil29@gmail.com>
2023-09-16 17:46:26 +0530

    backend

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import pyrebase
+import uuid
+import datetime
+
+app = Flask(__name__)
+
+config = {
+    "apiKey": "AIzaSyCGCrFViSsursoizS3Dgssbsr70Kd3zOYw",
+    "authDomain": "onwords-master-api-db.firebaseapp.com",
+    "projectId": "onwords-master-api-db",
+    "storageBucket": "onwords-master-api-db.appspot.com",
+    "messagingSenderId": "812641457896",
+    "appId": "1:812641457896:web:5226da7ccadaad50009b5d",
+    "measurementId": "G-4TW59PDLHQ",
+    "databaseURL":"https://onwords-master-api-db-default-rtdb.asia-southeast1.firebasedatabase.app"
+    }
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+print("db",db)
+auth = firebase.auth()
+
+#Define the directory where firmware files will be stored
+firmware_directory = '/firmware/3chfb'
+
+
+# ...
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    error_message = None
+    device_data = {}
+
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+
+        if not product_id:
+            error_message = "Product ID is required."
+        else:
+            # Check if the provided product ID exists in the database
+            device_data = db.child("Devices").child(product_id).get().val()
+
+            if not device_data:
+                error_message = "Product ID not found."
+
+    return render_template('home.html', device_data=device_data, error_message=error_message)
+
+@app.route('/upload_firmware', methods=['POST'])
+def upload_firmware():
+    product_id = request.form.get('product_id')
+
+    if not product_id:
+        return redirect(url_for('home'))
+
+    firmware_file = request.files['firmware_file']
+
+    if firmware_file:
+        # Create the firmware directory if it doesn't exist
+        os.makedirs(firmware_directory, exist_ok=True)
+
+        # Save the uploaded file to the specified directory
+        new_firmware_ref = db.child("Devices").child(product_id).child("firmware").push({
+            "filename": firmware_file.filename,
+            "upload_date": datetime.date.today().strftime("%Y-%m-%d"),
+            "version": "1.1.1"
+        })
+
+        # Get the generated key from the reference
+        firmware_uid = new_firmware_ref["name"]
+
+        firmware_file.save(os.path.join(firmware_directory, firmware_uid))
+        print(f"Firmware file '{firmware_uid}' saved to {firmware_directory}")
+
+    return redirect(url_for('home'))
+
+@app.route('/filter_devices', methods=['POST','GET'])
+def filter_devices():
+    product_id_prefix = request.form.get('product_id_prefix')
+
+    if not product_id_prefix:
+        return render_template('home.html', error_message="Invalid request")
+
+    # Get devices with product IDs starting with the specified prefix
+    devices = db.child("Devices").get().val()
+    filtered_devices = {}
+
+    if devices:
+        for device_id, device_data in devices.items():
+            if device_id.startswith(product_id_prefix):
+                filtered_devices[device_id] = device_data
+              
+    print("filter",filtered_devices)
+    if filtered_devices:
+        return render_template('threechannel.html', device_data=filtered_devices)
+    else:
+        return render_template('threechannel.html', error_message="No devices found for the selected module")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
